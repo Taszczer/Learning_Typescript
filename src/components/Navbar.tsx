496efebb6b63f59a0f5264b3054af01af44c58a9
lolkek1234567890 <dima2019mega@gmail.com>
2023-06-28 19:12:31 +0200

    The start

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+import { Routes, Route } from "react-router-dom"
+import { navLinks } from "../data"
+
+const Navbar = () => {
+    return <Routes></Routes>
+}
+
+export default Navbar
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import Navbar from "./components/Navbar.tsx"
+import { BrowserRouter } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+            <Navbar />
+        </BrowserRouter>
+    </React.StrictMode>
 )
