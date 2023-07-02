c04959590caa5335514eb2ab15161bae6f5bf5f4
lolkek1234567890 <dima2019mega@gmail.com>
2023-07-02 17:32:54 +0200

    Learning

--- src/components/4lesson.ts
+++ src/components/4lesson.ts
@@ -83,10 +83,12 @@ const infinite = () => {
     }
 }
 
+//custom type guard
 const isNumber = (value: any): boolean => {
     return typeof value === "number" ? true : false
 }
 
+//use of the b=never type
 const numberOrString = (value: number | string): string => {
     if (typeof value === "string") return "string"
     if (isNumber(value)) return "number"
