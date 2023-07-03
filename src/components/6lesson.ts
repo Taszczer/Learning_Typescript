5c666eb284bd7b1defb000ec582edb8854a960f3
lolkek1234567890 <dima2019mega@gmail.com>
2023-07-03 18:01:37 +0200

    Learning

--- src/components/6lesson.ts
+++ src/components/6lesson.ts
@@ -1,13 +1,24 @@
 class Coder {
+    secondLang!: string //stay away from this XD
+
     constructor(
         public readonly name: string,
         public music: string,
         private age: number,
-        protected lang: string
+        protected lang: string = "Ukrainian"
     ) {
         this.name = name
         this.music = music
         this.age = age
         this.lang = lang
     }
+
+    public getAge() {
+        return `I'm ${this.age}`
+    }
 }
+
+const Dima = new Coder("Dima", "Phonk", 16)
+console.log(Dima.getAge())
+console.log(Dima.age)
+console.log(Dima.lang) //I have error but it's work. Error because lang is protected
