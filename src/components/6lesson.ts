1a5eea3dd6c78fa1c99fe38cc8a583ee96970713
lolkek1234567890 <dima2019mega@gmail.com>
2023-07-06 17:03:08 +0200

    Learning

--- src/components/6lesson.ts
+++ src/components/6lesson.ts
@@ -1,11 +1,13 @@
+import { act } from "react-dom/test-utils"
+
 class Coder {
-    secondLang!: string //stay away from this
+    secondLang!: string //stay away from this XD
 
     constructor(
         public readonly name: string,
         public music: string,
         private age: number,
-        protected lang: string = "Ukrainian"
+        protected lang: string = "Typescript"
     ) {
         this.name = name
         this.music = music
@@ -32,5 +34,51 @@ class WebDev extends Coder {
     ) {
         super(name, music, age)
         this.computer = computer
+    }
+
+    public getLang() {
+        return `I write this website using ${this.lang}`
+    }
+}
+const myLang = new WebDev("XX", "Dima", "Phonk", 16)
+console.log(myLang.getLang())
+// console.log(myLang.age)
+
+interface Musician {
+    name: string
+    instrument: string
+    play(action: string): string
+}
+
+class Guitarist implements Musician {
+    name: string
+    instrument: string
+
+    constructor(name: string, instrument: string) {
+        this.name = name
+        this.instrument = instrument
+    }
+
+    play(action: string): string {
+        return `${this.name} ${action} the ${this.instrument}`
+    }
+}
+const Page = new Guitarist("Boby", "guitar")
+console.log(Page.play("strums"))
+
+///////////////////////////////////////////
+
+class Peeps {
+    static count: number = 0
+
+    static getCount(): number {
+        return Peeps.count
+    }
+
+    public id: number
+
+    constructor(public name: string) {
+        this.name = name
+        this.id = ++Peeps.count
     }
 }
