5c61998c66337ac8658faa504776d734e59a85c1
lolkek1234567890 <dima2019mega@gmail.com>
2023-07-07 13:48:03 +0200

    Learning

--- src/components/7lesson.ts
+++ src/components/7lesson.ts
@@ -1,6 +1,11 @@
 //Index Signatures
 
+// interface TransactionObj {
+//     readonly [index: string]: number
+// }
+
 interface TransactionObj {
+    readonly [index: string]: number
     Pizza: number
     Books: number
     Job: number
@@ -25,3 +30,30 @@ console.log(todaysTr["Pizza"])
 
 let prop: string = "Pizza"
 console.log(todaysNet(todaysTr))
+
+console.log(todaysTr["Dima"])
+
+/////////////////////////////////
+
+interface Student {
+    //[key: string]: string | number | number[] | undefined
+    name: string
+    GPA: number
+    classes?: number[]
+}
+
+const student: Student = {
+    name: "Dima",
+    GPA: 3.5,
+    classes: [100, 200],
+}
+
+// console.log(student.test)
+
+for (const key in student) {
+    console.log(`${key}: ${student[key as keyof Student]}`)
+}
+
+Object.keys(student).map((key) => {
+    console.log(student[key as keyof typeof student])
+})
